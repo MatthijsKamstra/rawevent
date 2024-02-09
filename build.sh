e2329e18d256444daadb8381591677a285a4759e
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2024-02-09 21:36:34 +0100

    lets try this

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+GITHUB_USER=matthijskamstra
+PROJECT=rawevent
+
+# Build the project first.
+ng build --configuration production --base-href=/$PROJECT
+
+# Instead of copying index.html, echo and cat the content so that `YAML front matter` was added.
+echo "---
+permalink: /404.html
+---
+" > "./dist/$PROJECT/404.html"
+cat "./dist/$PROJECT/index.html" >> "./dist/$PROJECT/404.html"
