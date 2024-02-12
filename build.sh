b6bdd8e1e65acd415b0fff760289aef4a3d34fd5
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2024-02-12 17:13:10 +0100

    ☢️ try again

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ GITHUB_USER=matthijskamstra
 PROJECT=rawevent
 
 # Build the project first.
-ng build --configuration production --deploy-url=https://$GITHUB_USER.github.io/$PROJECT/ --base-href=/$PROJECT/
+# ng build --configuration production --deploy-url=https://$GITHUB_USER.github.io/$PROJECT/ --base-href=/$PROJECT/
+ng build --configuration production --base-href https://$GITHUB_USER.github.io/$PROJECT/
 
 # Instead of copying index.html, echo and cat the content so that `YAML front matter` was added.
 echo "---
